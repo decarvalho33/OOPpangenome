7d39f22b06230019dc32058b5da3fc5523549237
decarvalho33 <alvarodecarvalho33@gmail.com>
2023-09-16 19:57:13 -0300

    first commit

---
+++ src/main/Main.java
@@ -0,0 +1,11 @@
+package main;
+
+public class Main {
+
+	public static void main(String[] args) {
+		double decimal = 10.5;
+		int inteiro = (int) decimal;
+		System.out.println(inteiro);
+	}
+
+}
---
+++ src/ooppangenome/models/repositories/GenBank.java
@@ -0,0 +1,10 @@
+package repositories;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class GenBank {
+	List<String> genes = new ArrayList<>();
+	
+	public <T> gene
+}
